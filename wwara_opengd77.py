bd1a0bfda46f32da882b0a16971a1a0090a798f0
Andrew Jorgensen <andrew@jorgensenfamily.us>
2023-09-23 16:38:17 -0700

    Update opengd77 with location

--- wwara_opengd77.py
+++ wwara_opengd77.py
@@ -1,284 +1,299 @@
 #!/usr/bin/env python
 """Converts a WWARA database dump to GB3GF CSV format for GD-77."""
-import codecs
 import logging
-from csv import DictReader, DictWriter
+import re
+from csv import DictWriter
 from decimal import Decimal
-from io import BytesIO
 from sys import stdout, stderr
-from zipfile import ZipFile
+
+from channel import Channel
+from wwara.database import coordinations
 
 LOG = logging.getLogger(__name__)
 
-FIELDNAMES = (
-    "Data type",
-    "Name",
-    "Channel Type",
-    "Rx Frequency",
-    "Tx Frequency",
-    "Color Code",
-    "Timeslot",
-    "Contact",
-    "Contact Type",
-    "Contact Id",
-    "Rx Group",
-    "Scanlist",
-    "Zone",
-    "RX CTCSS",
-    "TX CTCSS",
-    "Power",
-    "Bandwidth",
-    "Rx Only",
-    "Squelch",
-    "Tx Admit",
-    "TOT",
-    "TOT Rekey",
-    "Tx Signaling",
-    "Rx Signaling",
-    "Privacy Group",
-    "Emergency System",
-    "Flags1",
-    "Flags2",
-    "Flags3",
-    "Flags4",
-    "RssiThreshold",
-    "VoiceEmphasis",
-    "TxSignaling",
-    "UnmuteRule",
-    "RxSignaling",
-    "ArtsInterval",
-    "Optional DMRID",
-)
+LAT = Decimal(47.80)
+LON = Decimal(-122.25)
+RANGE = 80
+
+NAME_LENGTH = 16
+DELIMITER = ","
 
 
-def _drop_decimals(decimal):
-    """Decimal.normalize gives 2E+1 for 20..."""
-    decimal = str(decimal)
-    if "." in decimal:
-        decimal = decimal.rstrip("0").rstrip(".")
-    return decimal
+class GB3GFChannel(Channel):
+    number_k = "Channel Number"
+    name_k = "Channel Name"
+    channel_type_k = "Channel Type"
+    rx_frequency_k = "Rx Frequency"
+    tx_frequency_k = "Tx Frequency"
+    bandwidth_k = "Bandwidth (kHz)"
+    colour_code_k = "Colour Code"
+    timeslot_k = "Timeslot"
+    contact_k = "Contact"
+    tg_list_k = "TG List"
+    dmr_id_k = "DMR ID"
+    ts1_ta_tx_k = "TS1_TA_Tx"
+    ts2_ta_tx_k = "TS2_TA_Tx ID"
+    rx_tone_k = "RX Tone"
+    tx_tone_k = "TX Tone"
+    squelch_k = "Squelch"
+    power_k = "Power"
+    rx_only_k = "Rx Only"
+    zone_skip_k = "Zone Skip"
+    all_skip_k = "All Skip"
+    tot_k = "TOT"
+    vox_k = "VOX"
+    no_beep_k = "No Beep"
+    no_eco_k = "No Eco"
+    aprs_k = "APRS"
+    latitude_k = "Latitude"
+    longitude_k = "Longitude"
+    fieldnames = (
+        number_k,
+        name_k,
+        channel_type_k,
+        rx_frequency_k,
+        tx_frequency_k,
+        bandwidth_k,
+        colour_code_k,
+        timeslot_k,
+        contact_k,
+        tg_list_k,
+        dmr_id_k,
+        ts1_ta_tx_k,
+        ts2_ta_tx_k,
+        rx_tone_k,
+        tx_tone_k,
+        squelch_k,
+        power_k,
+        rx_only_k,
+        zone_skip_k,
+        all_skip_k,
+        tot_k,
+        vox_k,
+        no_beep_k,
+        no_eco_k,
+        aprs_k,
+        latitude_k,
+        longitude_k,
+    )
+    name_length = NAME_LENGTH
+
+    def __init__(self, channel):
+        super().__init__(
+            channel.call,
+            channel.output,
+            channel.input,
+            bandwidth=channel.bandwidth,
+            modes=channel.modes,
+            output_tone=channel.output_tone,
+            input_tone=channel.input_tone,
+            output_code=channel.output_code,
+            input_code=channel.input_code,
+            dmr_cc=channel.dmr_cc,
+            location=channel.location,
+            latitude=channel.latitude,
+            longitude=channel.longitude,
+            rx_only=channel.rx_only,
+        )
+        self._name = None
+        self._number = 0
+
+    @property
+    def _channel_type(self):
+        mode = "Analogue"
+        if "DMR" in self.modes:
+            mode = "Digital"
+        return mode
+
+    @property
+    def _rx_frequency(self):
+        return f"{Decimal(self.output):.5f}"
+
+    @property
+    def _tx_frequency(self):
+        return f"{Decimal(self.input):.5f}"
+
+    @classmethod
+    def _tone(cls, tone, code):
+        if not tone:
+            if not code:
+                return "None"
+            return f"D{code}N"
+        return f"{Decimal(tone):.1f}"
+
+    @property
+    def _rx_tone(self):
+        return self._tone(self.output_tone, self.output_code)
+
+    @property
+    def _tx_tone(self):
+        return self._tone(self.input_tone, self.input_code)
+
+    @property
+    def _bandwidth(self):
+        if "DMR" in self.modes:
+            return None
+        return self.bandwidth
+
+    @property
+    def _rx_only(self):
+        if self.rx_only:
+            return "Yes"
+        return "No"
+
+    def items(self):
+        yield self.number_k, self.number
+        yield self.name_k, self.name
+        yield self.channel_type_k, self._channel_type
+        yield self.rx_frequency_k, self._rx_frequency
+        yield self.tx_frequency_k, self._tx_frequency
+        yield self.bandwidth_k, self._bandwidth
+        yield self.colour_code_k, self.dmr_cc or 0
+        yield self.timeslot_k, 1
+        yield self.contact_k, None
+        yield self.tg_list_k, None
+        yield self.dmr_id_k, None
+        yield self.ts1_ta_tx_k, "Off"  # or "Text" or "APRS" or "APRS & Text"
+        yield self.ts2_ta_tx_k, "Off"
+        yield self.rx_tone_k, self._rx_tone
+        yield self.tx_tone_k, self._tx_tone
+        yield self.squelch_k, "Disabled"
+        yield self.power_k, "Master"
+        yield self.rx_only_k, self._rx_only
+        yield self.zone_skip_k, "No"
+        yield self.all_skip_k, "No"
+        yield self.tot_k, 0
+        yield self.vox_k, "Off"
+        yield self.no_beep_k, "No"
+        yield self.no_eco_k, "No"
+        yield self.aprs_k, "None"
+        yield self.latitude_k, self.latitude
+        yield self.longitude_k, self.longitude
 
 
-def _supported(row):
-    """Checks if the mode is supported."""
-    if "Y" not in (row["DMR"], row["FM_WIDE"], row["FM_NARROW"]):
+def _supported(channel):
+    """Checks if the channel is supported by the radio."""
+    if "DMR" not in channel.modes and "FM" not in channel.modes:
         return False
-    ifreq = Decimal(row["INPUT_FREQ"])
-    if ifreq > 144 and ifreq < 148:
-        # 2M
+    if 144 <= channel.input <= 148:
         return True
-    if ifreq > 420 and ifreq < 450:
-        # 70CM
+    if 222 <= channel.input <= 225:
+        channel.rx_only = True
+        return True
+    if 420 <= channel.input <= 450:
         return True
     return False
 
 
-def _name(row, prefix=""):
-    """Formats a usable name for the repeater."""
-    length = 16 - len(prefix)
-    name = prefix + " ".join((row["CALL"], row["CITY"]))[:length]
-    return name
-
-
-def _channel_type(row):
-    """Converts the mode per WWARA to the Channel Type per GD-77"""
-    mode = "Analog"
-    if row["DMR"] == "Y":
-        mode = "Digital"
-    elif row["FM_WIDE"] == "Y":
-        mode = "Analog"
-    elif row["FM_NARROW"] == "Y":
-        mode = "Analog"
-    return mode
-
-
-def _color_code(row):
-    """Returns the DMR Color Code from the WWARA record"""
-    color_code = row.get("DMR_COLOR_CODE").lstrip("CC")
-    return color_code or "0"
-
-
-def _zone(row):
-    # TODO Zones have limits and the CPS fails import when it reaches them
-    if _channel_type(row) == "Digital":
-        return "DMR"
-    else:
-        return "None"
-
-
-def _ctcss(tone):
-    if not tone:
-        return "None"
-    return "{:.1f}".format(Decimal(tone))
-
-
-def _rx_ctcss(row):
-    return _ctcss(row.get("CTCSS_OUT"))
-
-
-def _tx_ctcss(row):
-    return _ctcss(row.get("CTCSS_IN"))
-
-
-def _bandwidth(row):
-    if row.get("FM_WIDE") == "Y":
-        return "25"
-    elif "Y" in (row.get("DMR"), row.get("FM_NARROW")):
-        return "12.5"
-
-
-def _entry(row, prefix=""):
-    name = _name(row, prefix)
-    channel_type = _channel_type(row)
-    rx_frequency = "{:.5f}".format(Decimal(row["OUTPUT_FREQ"]))
-    tx_frequency = "{:.5f}".format(Decimal(row["INPUT_FREQ"]))
-    color_code = _color_code(row)
-    zone = _zone(row)
-    rx_ctcss = _rx_ctcss(row)
-    tx_ctcss = _tx_ctcss(row)
-    bandwidth = _bandwidth(row)
-    # Bits (low to high)
-    # 0|0x01: Squelch
-    # 1|0x02: 25 KHz
-    # 2|0x04: RX Only
-    # 3|0x08:
-    # 4|0x10: All Skip
-    # 5|0x20: Zone Skip
-    # 6|0x40: Vox
-    # 7|0x80: Power
-    # 1   = 00000001: LP 12.5 KHz
-    # 17  = 00010001: LP 12.5 KHz All-skip
-    # 129 = 10000001: HP 12.5 KHz
-    # 131 = 10000011: HP 25 KHz
-    # 135 = 10000111: HP 25 KHz Rx-only
-    # 151 = 10010111: HP 25 KHz Rx-only All-skip
-    flags = 0x80 | 0x01
-    if bandwidth == "25":
-        flags |= 0x02
-    return {
-        "Data type": "CH_DATA",
-        "Name": name,
-        "Channel Type": channel_type,
-        "Rx Frequency": rx_frequency,
-        "Tx Frequency": tx_frequency,
-        "Color Code": color_code,
-        "Timeslot": 1,
-        "Contact": "N/A",
-        "Contact Type": 0,
-        "Contact Id": "N/A",
-        "Rx Group": "None",
-        "Scanlist": "None",
-        "Zone": zone,
-        "RX CTCSS": rx_ctcss,
-        "TX CTCSS": tx_ctcss,
-        "Power": "Master",
-        "Bandwidth": bandwidth,
-        "Rx Only": "No",
-        "Squelch": "Master",
-        "Tx Admit": 0,
-        "TOT": 0,
-        "TOT Rekey": 0,
-        "Tx Signaling": 0,
-        "Rx Signaling": 0,
-        "Privacy Group": 0,
-        "Emergency System": 0,
-        "Flags1": 0,
-        "Flags2": 0,
-        "Flags3": 0,
-        "Flags4": flags,
-        "RssiThreshold": -80,
-        "VoiceEmphasis": 0,
-        "TxSignaling": 0,
-        "UnmuteRule": 0,
-        "RxSignaling": 0,
-        "ArtsInterval": 0,
-        "Optional DMRID": None,
-    }
-
-
-def _order(name):
-    if "-pending-" in name:
-        return -3
-    elif "-rptrlist-" in name:
-        return -4
-    elif "-About2Expire-" in name:
-        return -2
-    elif "-Expired-" in name:
-        return -1
-    return 0
-
-
-def _dedup_names(wlist):
+def _dedup_names(
+    elist,
+    namek="Channel Name",
+    typek="Channel Type",
+    digital="Digital",
+    outputk="Rx Frequency",
+):
     names = {}
-    for entry in wlist:
-        if entry["Name"] in names:
-            names[entry["Name"]]["entries"].append(entry)
+    for entry in elist:
+        if entry[namek] in names:
+            names[entry[namek]]["entries"].append(entry)
         else:
-            names[entry["Name"]] = {"entries": [entry]}
-            names[entry["Name"]]["dups"] = {"70cm": 0, "2m": 0, "DMR": 0}
-        if entry["Channel Type"] == "Digital":
-            names[entry["Name"]]["dups"]["DMR"] += 1
-        if entry["Rx Frequency"].startswith("1"):
-            names[entry["Name"]]["dups"]["2m"] += 1
-        if entry["Rx Frequency"].startswith("4"):
-            names[entry["Name"]]["dups"]["70cm"] += 1
-    for entry in sorted(wlist, key=lambda x: (x["Name"], Decimal(x["Rx Frequency"]))):
-        if len(names[entry["Name"]]["entries"]) > 1:
-            freq = entry["Rx Frequency"].rstrip("0").rstrip(".")
-            dups = names[entry["Name"]]["dups"]
-            if dups["DMR"] == 1 and entry["Channel Type"] == "Digital":
-                tag = "|D"
-            elif dups["DMR"] == 1 and dups["2m"] <= 1 and dups["70cm"] <= 1:
-                tag = ""
-            elif dups["70cm"] == 1 and entry["Rx Frequency"].startswith("4"):
-                tag = "70cm"
-            elif dups["2m"] == 1 and entry["Rx Frequency"].startswith("1"):
-                tag = "2m"
-            elif (dups["70cm"] > 1 and entry["Rx Frequency"].startswith("4")) or (
-                dups["2m"] > 1 and entry["Rx Frequency"].startswith("1")
+            names[entry[namek]] = {"entries": [entry]}
+            names[entry[namek]]["dups"] = {
+                "UHF": 0,
+                "220": 0,
+                "VHF": 0,
+                "DMR": 0,
+            }
+        if entry[typek] == digital:
+            names[entry[namek]]["dups"]["DMR"] += 1
+        if entry[outputk].startswith("1"):
+            names[entry[namek]]["dups"]["VHF"] += 1
+        if entry[outputk].startswith("2"):
+            names[entry[namek]]["dups"]["220"] += 1
+        if entry[outputk].startswith("4"):
+            names[entry[namek]]["dups"]["UHF"] += 1
+    for entry in sorted(elist, key=lambda x: (x[namek], Decimal(x[outputk]))):
+        if len(names[entry[namek]]["entries"]) > 1:
+            freq = entry[outputk].rstrip("0").replace(".", "")[2:]
+            dups = names[entry[namek]]["dups"]
+            if dups["DMR"] == 1 and entry[typek] == digital:
+                tag = "D"
+            elif dups["UHF"] == 1 and entry[outputk].startswith("4"):
+                tag = "U"
+            elif dups["220"] == 1 and entry[outputk].startswith("2"):
+                tag = "2"
+            elif dups["VHF"] == 1 and entry[outputk].startswith("1"):
+                tag = "V"
+            elif (
+                (dups["UHF"] > 1 and entry[outputk].startswith("4"))
+                or (dups["220"] > 1 and entry[outputk].startswith("2"))
+                or (dups["VHF"] > 1 and entry[outputk].startswith("1"))
             ):
                 tag = freq
-            length = 16 - len(tag)
-            entry["Name"] = entry["Name"][:length].rstrip() + tag
+            length = NAME_LENGTH - len(tag)
+            entry[namek] = re.sub(
+                " +", " ", entry[namek].ljust(NAME_LENGTH)[:length] + tag
+            )
+    seen = set()
+    for entry in elist:
+        if entry[namek] in seen:
+            print(
+                f"BUG! Duplicate names still exist! {entry['Channel Name']}",
+                file=stderr,
+            )
+        else:
+            seen.add(entry[namek])
 
 
-def convert(zipfile):
-    """Converts a WWARA zipfile."""
-    wlist = []
-    for name in sorted(zipfile.namelist(), key=_order):
-        if name.endswith(".csv"):
-            print(name, file=stderr)
-            prefix = ""
-            if "-pending-" in name:
-                prefix = "+"
-            elif "-About2Expire-" in name:
-                prefix = "-"
-                continue
-            elif "-Expired-" in name:
-                prefix = "!"
-                continue
-            with zipfile.open(name, "r") as csv:
-                # Remove the DATA_SPEC_VERSION header line from the .csv
-                csv.readline()
-                for row in DictReader(codecs.getreader("us-ascii")(csv)):
-                    if not _supported(row):
-                        continue
-                    wlist.append(_entry(row, prefix))
-    _dedup_names(wlist)
-    return sorted(wlist, key=lambda x: (x["Channel Type"], Decimal(x["Rx Frequency"])))
+def channels_csv(channels):
+    with open("Channels.csv", "w", newline="") as _channels_csv:
+        writer = DictWriter(
+            _channels_csv, fieldnames=GB3GFChannel.fieldnames, delimiter=DELIMITER
+        )
+        writer.writeheader()
+        writer.writerows(channels)
+
+
+ZONES_FIELDNAMES = tuple(["Zone Name"] + ["Channel " + str(i) for i in range(1, 81)])
+
+
+def zones_csv(channels):
+    zones = {
+        "WWARA": {"mode": None, "low": 144, "high": 450},
+        "WWARA FM": {"mode": "FM", "low": 144, "high": 450},
+        "WWARA DMR": {"mode": "DMR", "low": 144, "high": 450},
+        "WWARA VHF": {"mode": "FM", "low": 144, "high": 148},
+        "WWARA 220": {"mode": "FM", "low": 222, "high": 225},
+        "WWARA UHF": {"mode": "FM", "low": 420, "high": 450},
+    }
+    with open("Zones.csv", "w", newline="") as _zones_csv:
+        writer = DictWriter(
+            _zones_csv, fieldnames=ZONES_FIELDNAMES, delimiter=DELIMITER
+        )
+        writer.writeheader()
+        for name, spec in zones.items():
+            zone = {"Zone Name": name}
+            i = 1
+            for channel in channels:
+                if i > 80:
+                    break
+                if (spec["mode"] is not None) and (spec["mode"] not in channel.modes):
+                    continue
+                if not (spec["low"] <= channel.input <= spec["high"]):
+                    continue
+                zone[f"Channel {i}"] = channel.name
+                i += 1
+            writer.writerow(zone)
 
 
 if __name__ == "__main__":
-    from urllib.request import urlopen
-
-    with urlopen("https://www.wwara.org/DataBaseExtract.zip") as RESPONSE:
-        # ZipFile requires a file-like object that supports seek
-        FILE_OBJ = BytesIO(RESPONSE.read())
-    ZIPFILE = ZipFile(FILE_OBJ)
-
-    WRITER = DictWriter(stdout, FIELDNAMES)
-    WRITER.writeheader()
-
-    WRITER.writerows(convert(ZIPFILE))
-
-    FILE_OBJ.close()
+    channels = []
+    for channel in coordinations():
+        if not _supported(channel):
+            continue
+        channels.append(GB3GFChannel(channel))
+    _dedup_names(channels)
+    # Sort channels in order of output frequency
+    channels_csv(sorted(channels, key=lambda channel: channel.output))
+    # Sort channels in zones in order of distance (closest first)
+    zones_csv(sorted(channels, key=lambda channel: channel.distance(LAT, LON)))
